84aed0cb50530b7ae1f7e9a09dc4c4eb2880d400
SP <8068616+sp-hk2ldn@users.noreply.github.com>
2026-03-24 12:53:27 +0000

    Update searchads formula to v0.2.1

--- Formula/searchads.rb
+++ Formula/searchads.rb
@@ -1,8 +1,8 @@
 class Searchads < Formula
   desc "Apple Search Ads CLI"
   homepage "https://github.com/sp-hk2ldn/searchads-cli"
-  url "https://github.com/sp-hk2ldn/searchads-cli/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "66fda849e20902683cb5710cd766f903f3c80931a5792c97ab9639a8eb0bdf82"
+  url "https://github.com/sp-hk2ldn/searchads-cli/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "d77c93ab6781ca0d00ba8552019170d7639263d5bfa273f2b8b0a1e4914a89c3"
   license "MIT"
 
   depends_on "go" => :build
